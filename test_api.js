ed0dab339221ffd6fa9a8516b5a81181bd5806bc
HanzalaKamran1234 <growtoglow44@gmail.com>
2026-04-11 15:58:25 +0500

    Remove debug scripts

--- test_api.js
+++
@@ -1,9 +0,0 @@
-async function test() {
-  const res = await fetch('http://localhost:3000/api/upload/signature', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ folder: 'ar_saas/test' })
-  });
-  console.log(await res.text());
-}
-test();
